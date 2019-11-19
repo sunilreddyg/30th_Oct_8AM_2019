df07d8eecbc08ca1c87939fc253fa1fe30161092
Administrator <Administrator@192.168.1.9>
2019-11-19 09:42:29 +0530

    Dropdown Selections

--- Project_175/src/selenium_webdriver/Dropdown/Dropdown_Selection.java
+++ Project_175/src/selenium_webdriver/Dropdown/Dropdown_Selection.java
@@ -22,7 +22,18 @@ public class Dropdown_Selection {
 		Thread.sleep(3000);  //Static timeout to load cities
 		new Select(driver.findElement(By.id("customCity")))
 		.selectByValue("hyderabad");
+		
+		driver.findElement(By.id("customLocality")).clear();
+		driver.findElement(By.id("customLocality")).sendKeys("Dilsukhnagar");
 
+		//Select dropdown option using index number
+		new Select(driver.findElement(By.id("customRadius"))).selectByIndex(4);
+		
+		//Select checkbox [We use click command to select & deselect checkbox]
+		driver.findElement(By.id("amenity_category_order_types49")).click();
+		
+		//Click search button using xpath locator
+		driver.findElement(By.xpath("//input[@value='SEARCH']")).click();
 	}
 
 }
